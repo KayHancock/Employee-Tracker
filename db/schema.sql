3f1e8b73a280ba57cd24dc9e57d2f5b9d77544da
KayHancock <106179836+KayHancock@users.noreply.github.com>
2022-11-12 02:18:01 -0500

    added schema

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+
+USE employees;
+
+CREATE TABLE department (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) 
+);
+
+CREATE TABLE role (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
+);
